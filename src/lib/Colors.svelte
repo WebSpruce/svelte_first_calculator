7d80d87ac919c38ececd681f5bc670907f101d8d
WebSpruce <kacperswi12363@gmail.com>
2022-11-08 10:06:36 +0100

    add_colors_function

---
+++ src/lib/Colors.svelte
@@ -0,0 +1,11 @@
+<script>
+    var random = 0;
+    const randomstart=()=>{
+        let min=Math.ceil(1);
+        let max=Math.ceil(100);
+        random = Math.floor(Math.random()*(max-min)+min);
+    }
+</script>
+
+<h3>random number : {random}</h3>
+<input type="button" on:click={randomstart} value="generate random number"/>
