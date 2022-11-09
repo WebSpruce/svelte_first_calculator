07fd7e48544768b6c1662207134be90afebbface
WebSpruce <kacperswi12363@gmail.com>
2022-11-09 15:28:12 +0100

    add_guess

--- src/lib/Zg.svelte
+++ src/lib/Zg.svelte
@@ -1,5 +1,5 @@
 <script>
-    let word = ""; let word2=""; let index = 0;
+    let word = ""; let word2=""; let index = 0; let result=""; let f=0; let guess=false;
     const generaterandom=()=>{
         let words=["banana","apple","orange","melon","pumpkin"];
         let min = Math.ceil(0);
@@ -11,10 +11,61 @@
             word2+="*";
         }
     }
+    const guessfunction=(e)=>{
+        function replaceChar(origString, replaceChar, index)
+        {
+            let firstPart = origString.substr(0, index);
+            let lastPart = origString.substr(index + 1);
+            let newString =
+                firstPart + replaceChar + lastPart;
+            return newString;
+        }
+
+        if(e.key==='Enter'){
+            console.log("E : "+e.target.value);
+            for(let i=0; i<word.length; i++){
+                if(e.target.value == word[i]){
+                    console.log("OK : "+e.target.value);
+                    word2=replaceChar(word2,e.target.value,i);
+                    guess=true;
+                }else{
+
+                    guess=false;
+                }
+            }
+
+            if(guess==false){
+                f++;
+            }
+
+
+            e.target.value="";
+            if(!word2.includes("*")){
+                console.log("YES!");
+                result="YOU WON!";
+            }
+
+        }
+
+    }
+    const reset=()=>{
+        result="";
+        word2="";
+        generaterandom();
+    }
 </script>
 
 <h3>Word: {word2}</h3>
+<h6>f: {f}</h6>
 
 <input type="button" value="generate word" on:click={generaterandom}>
 
-<input type="text" on:keydown={e=>{if(e.target.value )}}>
+<input type="text" maxlength="1" on:keydown={guessfunction}>
+<h3>{result}</h3>
+<input type="button" value="generate word" on:click={reset}>
+
+<style>
+    h6{
+        color:indianred;
+    }
+</style>
